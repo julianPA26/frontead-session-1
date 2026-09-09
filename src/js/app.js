db97681c2d25315f8ee6561d17220003c7bae319
julianparra26 <julian.parra@ucaldas.edu.co>
2026-09-09 11:57:05 -0500

    Subiendo Cambios

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+console.log("Acceíi al JS");
+function saludoFuncion(){
+    console.log("Hola desde la funcion");
+}
+
+const SALUDO_FLECHA = () => {
+    console.log("Hola desde la funcion flecha");
+}
+const SALUDO_DOS = function () {
+    console.log("Hola desde la funcion anonima");
+}
